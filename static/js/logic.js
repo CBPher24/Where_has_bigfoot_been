953367121729e16dba68135a467f794b9a1746c2
Dan D <dans1451@gmail.com>
2022-07-09 13:25:12 -0400

    there are wee bigfoots now!

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,14 +3,36 @@ function createMap(big_foot){
     //Define Variable for Tile Layer:
     var satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
+        maxZoom: 30,
+        center: [37.0902,95.7129],
+        zoom: 2,
+        
         id: "mapbox.satellite",
-        accessToken: API_KEY
+        accessToken: API_KEY,
+
+        
+     
+
     });
+    var streetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 30,
+        center: [37.0902,95.7129],
+        zoom: 2,
+        
+     });
+
+     var topoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+        maxZoom: 30,
+        
+     });
 // Define baseMaps Object to Hold Base Layers:
 
     var baseMaps = {
     "Satellite": satelliteMap,
+    "Street" : streetMap,
+    "Topographical" : topoMap
    
     };
 
@@ -37,6 +59,14 @@ function createMap(big_foot){
 function createMarkers(bfoot_data) {
     var bfoot_data = JSON.parse(bfoot_data)
     console.log(bfoot_data[0])
+    var bfIcon = L.icon({
+        iconUrl: 'https://cdn.pixabay.com/photo/2022/01/14/04/02/bigfoot-6936421__340.png',
+        
+    
+        iconSize:     [20, 43], // size of the icon
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
 // }
 var markers = L.markerClusterGroup();
 
@@ -45,7 +75,7 @@ for (var i = 0; i < bfoot_data.length; i++) {
     var lon = bfoot_data[i].data.Lon;
   var location = [lon, lat];
   if(location) {
-    markers.addLayer(L.marker([location[1],location[0]])
+    markers.addLayer(L.marker([location[1],location[0]], {icon: bfIcon})
     .bindPopup("<h3>" + bfoot_data[i].data.ObjectId + "<h3><h3>Description: " + bfoot_data[i].data.descriptio + "</h3>"));
   }
 }
